d60fef8ac45e2ad7dab341e2b42f68cc8d3021ba
IgorMushk <igor.mushketik@gmail.com>
2023-11-15 00:40:50 +0200

    node:fs

--- lesson-1/node-modules-2/index.js
+++ lesson-1/node-modules-2/index.js
@@ -1,4 +1,36 @@
 //console.log("Hello Word!!!");
 
 //setInterval(()=>console.log("Hello Word!"),1000);
-setTimeout(()=>console.log("Hello Word! -1000 ms"),1000);
+//setTimeout(()=>console.log("Hello Word! -1000 ms"),1000);
+
+// const fs = require("node:fs");
+
+// console.log(fs);
+
+/*
+const fs = require("node:fs/promises");
+
+// прикол - хак чтобы обойти
+(async() => {
+    const content = await fs.readFile('text.txt');
+    console.log(Buffer.from('Hello'));
+    console.log(content);
+    console.log(content.toString());
+})()
+*/
+// async await нужно оборачивать в try - catch
+
+// async function foo() {};
+// foo();
+
+
+const fs = require('node:fs');
+
+fs.readFile('text.txt', (error,data) => {
+console.log('error', error)
+console.log('data', data);
+})
+
+
+const text = fs.readFileSync('text.txt');
+console.log('readFileSync',text);
